7731bc19e6c3208b7737db572be9634db41c86f5
Alexandre Eher <alexandre@eher.com.br>
2019-12-04 12:12:59 +0100

    Update terminal used by awesome
    the parameters -l will ensure that the terminal will start as a login
    terminal.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -49,7 +49,7 @@ beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 --terminal = "x-terminal-emulator"
-terminal = "lxterminal"
+terminal = "lxterminal -l"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
